0259df11aac461df0c948929963f66f975fbe918
LeonardoBriguente <leobriguente27@gmail.com>
2025-07-13 17:38:15 -0300

    Criação de arquivo e adição do conteúdo sobre o estudo de variáveis e tipos de dados

---
+++ variaveis.js
@@ -0,0 +1,26 @@
+// DEFINIÇÃO DE VARIÁVEIS
+// var let e const
+
+// var = Variável global, não mais utilizado no JavaScript moderno
+// var = 18;
+
+// const = Constante, não pode ser alterada após a atribuição
+const name = "Leo";
+//const name = "joao"; // nome da variável
+// caso tente alterar, ocorrerá um erro
+
+let age = 18; // Variável local, pode ser alterada
+age = 19; // Alterando o valor da variável
+
+// TIPOS DE DADOS
+const string = "Este é um texto";
+const number = 22;
+const boolean = true
+const array = [];
+const object = {};
+const func = () => {}
+const date = new Date();
+
+// A comunidade utilizado o padrão camelCase para nomear variáveis
+// Exemplo: myName, userAge, productList
+
