db240b4c9a97731c8385639e7159a86e120f4aec
LeonardoBriguente <leobriguente27@gmail.com>
2025-07-20 19:03:44 -0300

    Adicionado explicação e operadores de comparação ao arquivo

---
+++ 06_operadores_comparacao.js
@@ -0,0 +1,12 @@
+// Os operadores de comparação servem para comparar 2 ou mais valores e sempre retornam um booleano
+
+// > Maior que
+// < Menor que
+// >= Maior ou igual a 
+// <= Menor ou igual a 
+
+// == Igual a
+// === Estritamente igual a
+// != Diferente de
+// !== Estritamente diferente de
+
