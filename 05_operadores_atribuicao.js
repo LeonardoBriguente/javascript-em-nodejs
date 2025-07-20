a470036e4a8d0b96faf8a443eb99f644a409496b
LeonardoBriguente <leobriguente27@gmail.com>
2025-07-20 17:48:21 -0300

    Feito comentários explicativos sobre os operadores mais usaddos

--- 05_operadores_atribuicao.js
+++ 05_operadores_atribuicao.js
@@ -1,1 +1,8 @@
 // São símbolos utilizados para atribuir um valor a uma determinada variável
+
+// Atribuição Simples (=): Esse operador DEFINI valor de uma variável;
+// Adição Combinada (+=): Esse operador ADICIONA valor a mais em uma variável
+// Subtração Combinada (-=): Esse operador RETIRA um valor de uma variável;
+// Multiplicação Combinada (*=): Esse operador MULTIPLICA o valor de uma variável;
+// Multiplicação Combinada (/=): Esse operador DIVIDI o valor de uma variável;
+
