7fec0ca67081cb08dc0dc070e0074ca0b343bdfd
LeonardoBriguente <leobriguente27@gmail.com>
2025-07-20 19:27:57 -0300

    Feito exemplos utilizando cada um dos comparadores aprendidos

--- 06_operadores_comparacao.js
+++ 06_operadores_comparacao.js
@@ -10,3 +10,11 @@
 // != Diferente de
 // !== Estritamente diferente de
 
+console.log(`5 é maior que 2? ${5 > 2}`); // true
+console.log(`11 é menor que 13? ${11  < 13}`); // true
+console.log(`22 é maior ou igual a 22? ${22 >= 22}`); // true
+console.log(`1 é menor ou igual a 3? ${1 < 3}`); // true
+console.log(`3 é igual a 3? ${3 == 3}`); // true
+console.log(`15 é diferente de 12? ${15 != 12}`); // true
+console.log(`"34" é estritamente igual a 34? ${"34" === 34}`); // false
+console.log(`"98" é estritamente diferente de "98"? ${"98" !== "98"}`); // false
