34c21591edf9e4a0adb20e9880f0ac362762ee5f
LeonardoBriguente <leobriguente27@gmail.com>
2025-07-27 19:08:33 -0300

    Adicionado uma explicação sobre cada operador

---
+++ 07_operadores_logicos.js
@@ -0,0 +1,12 @@
+// Operadores Lógicos do JS
+// && E (AND)
+// || OU (OR)
+// !  NÃO (NOT)
+
+// O operador && é utilizado quando **todas** as condições precisam ser verdadeiras para que algo aconteça
+
+// O operador || é utilizado quando **apenas uma** das condições precisa ser verdadeira para que algo aconteça
+
+// Utilizado para criar negações e verificar quando algo **não é verdadeiro**. Ele nega o valor da variável, ou seja, Se for true, vira false - e vice-versa
+
+// EXEMPLOS
