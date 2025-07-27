ba2c174a4cabbafd73af8ef4a50e96189cf85173
LeonardoBriguente <leobriguente27@gmail.com>
2025-07-27 19:40:48 -0300

    Anotado conceitos e estruturação do operador ternário

---
+++ 08_operador_ternario.js
@@ -0,0 +1,4 @@
+// O operador ternário nada mais é do que uma maneira resumida da estrutura de repetição IF, sendo estruturado da seguinte maneira:
+
+// (expressão) ? (se verdadeiro) : (se falso)
+
